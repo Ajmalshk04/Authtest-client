9b7fabb95ac215c80e65b222ba86f04e35ac67f4
Ajmalshk04 <shaikhajmal50@gmail.com>
2025-03-12 12:25:37 +0530

    url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -28,21 +28,21 @@
 
 // export default axiosInstance;
 
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || import.meta.env.REACT_APP_API_URL,
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 export const setCsrfToken = (csrfToken) => {
   if (csrfToken) {
-    axiosInstance.defaults.headers.common['X-CSRF-Token'] = csrfToken;
+    axiosInstance.defaults.headers.common["X-CSRF-Token"] = csrfToken;
   } else {
-    delete axiosInstance.defaults.headers.common['X-CSRF-Token'];
+    delete axiosInstance.defaults.headers.common["X-CSRF-Token"];
   }
 };
 
